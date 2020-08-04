04d3fba76063644d9ff226210096ba61d75404a6
WHUTdk <371685892@qq.com>
2020-08-05 00:15:42 +0800

    [update]增加图片处理工具类

--- personManage-business/src/main/java/com/dingkai/personManage/business/utils/PicUtil.java
+++ personManage-business/src/main/java/com/dingkai/personManage/business/utils/PicUtil.java
@@ -21,7 +21,7 @@ import java.net.URL;
 public class PicUtil {
 
     private static final int connectionTimeout = 5000;
-    private static final int readTimeout = 530000;
+    private static final int readTimeout = 30000;
 
     /**
      * 将本地图片文件转为base64编码
@@ -46,20 +46,6 @@ public class PicUtil {
     }
 
     /**
-     * 将http链接图片转为base64
-     */
-    public static String picToBase64ByHttp(String httpURL) throws IOException {
-        URL url = new URL(httpURL);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setReadTimeout(readTimeout);
-        conn.setConnectTimeout(connectionTimeout);
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36");
-        InputStream in = conn.getInputStream();
-        return picToBase64(in);
-    }
-
-    /**
      * 图片base64转为图片文件,ImageIO.write会压缩图片
      */
     public static void base64ToPic(String base64, String filePath) throws IOException {
@@ -74,6 +60,20 @@ public class PicUtil {
     }
 
     /**
+     * 将http链接图片转为base64
+     */
+    public static String httpUrlToBase64(String httpURL) throws IOException {
+        URL url = new URL(httpURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setReadTimeout(readTimeout);
+        conn.setConnectTimeout(connectionTimeout);
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36");
+        InputStream in = conn.getInputStream();
+        return picToBase64(in);
+    }
+
+    /**
      * http图片下载为本地图片
      */
     public static void httpUrlToPic(String httpUrl, String filePath) throws IOException {
@@ -83,7 +83,7 @@ public class PicUtil {
 
     public static void main(String[] args) throws IOException {
         String url1 = "http://hiphotos.qianqian.com/ting/pic/item/c83d70cf3bc79f3d98ca8e36b8a1cd11728b2988.jpg";
-        String base64 = picToBase64ByHttp(url1);
+        String base64 = httpUrlToBase64(url1);
         base64ToPic(base64, "C:/Users/86152/Downloads/test2.jpg");
         String url2 = "http://hiphotos.qianqian.com/ting/pic/item/8b13632762d0f7035cb3feda0afa513d2697c5b7.jpg";
         httpUrlToPic(url2, "C:/Users/86152/Downloads/test.jpg");
