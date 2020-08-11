722408d512964ad035bd351e5653c8698f7e762e
WHUTdk <371685892@qq.com>
2020-08-11 22:29:12 +0800

    [add]增加rabbitmq部分代码

--- personManage-business/src/main/java/com/dingkai/personManage/business/config/RedisConfig.java
+++ personManage-business/src/main/java/com/dingkai/personManage/business/config/RedisConfig.java
@@ -29,7 +29,7 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory);
 
         // 使用Jackson2JsonRedisSerialize 替换默认的jdkSerializable序列化
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
---
+++ personManage-web/src/test/java/com/dingkai/personManage/RabbitMqTest.java
@@ -0,0 +1,30 @@
+package com.dingkai.personManage;
+
+import com.dingkai.personManage.business.mq.RabbitMqProducer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author dingkai
+ * @Date 2020/8/11 22:15
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitMqTest {
+
+    @Autowired
+    private RabbitMqProducer rabbitMqProducer;
+
+    @Autowired
+    private AmqpTemplate rabbitMqTemplate;
+
+    @Test
+    public void producerTest(){
+        rabbitMqProducer.sendMessage();
+    }
+
+}
